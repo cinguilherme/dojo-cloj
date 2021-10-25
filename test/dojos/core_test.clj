279d99c5c19994c8a112145c126ca4b9597a4935
Pedro Evangelista <pedro.evangelista@gmail.com>
2021-10-25 10:57:12 -0300

    Create is-anagram? function

--- test/dojos/core_test.clj
+++ test/dojos/core_test.clj
@@ -5,9 +5,30 @@
             [matcher-combinators.matchers :as m]))
 
 
-(defn anagrams [word])
+(defn anagrams [word]
+  (let [reversed (apply str (reverse word))]
+    [reversed word]))
+
+(defn is-anagram? [first second]
+  (= (frequencies first)
+     (frequencies second)))
+
+(deftest is-anagram?-test
+  (testing "return true when is an anagram"
+    (is (true? (is-anagram? "limbo" "mobil")))
+    (is (true? (is-anagram? "limbo" "bolim")))
+    (is (false? (is-anagram? "limbo" "limbu")))
+    (is (false? (is-anagram? "limbo" "limbou")))))
 
 (deftest anagram-test
-  (testing "inveting anagram"
+  (testing "inverting anagram"
     (is (match? (m/embeds ["obmil"])
-                (anagrams "limbo")) )))
+                (anagrams "limbo"))))
+
+  (testing "includes itself"
+    (is (match? (m/embeds ["limbo"])
+                (anagrams "limbo"))))
+
+  (testing "rotate letters"
+    (is (match? (m/embeds ["olimb" "bolim" "mboli" "imbol"])
+                (anagrams "limbo")))))
