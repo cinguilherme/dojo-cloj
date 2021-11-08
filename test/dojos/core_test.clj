69ff936717c61185e289643e36f153fa70463277
Pedro Evangelista <pedro.evangelista@gmail.com>
2021-11-08 10:42:17 -0300

    Test two-words-of-same-size-as

--- test/dojos/core_test.clj
+++ test/dojos/core_test.clj
@@ -1,24 +1,28 @@
 (ns dojos.core-test
   (:require [clojure.test :refer :all]
-            [dojos.core :refer :all]
+            [dojos.anagram :as anagram]
             [matcher-combinators.test :refer [match?]]
             [matcher-combinators.matchers :as m]))
 
+;; https://codingdojo.org/kata/Anagram/
 
 (defn anagrams [word]
   (let [reversed (apply str (reverse word))]
     [reversed word]))
 
-(defn is-anagram? [first second]
-  (= (frequencies first)
-     (frequencies second)))
+(deftest two-words-of-same-size-as-test
+  (testing "Return two word tuples from the given dictionary with the combined size as the given word"
+    (is (match? (m/in-any-order [#{"ccc" "d"}
+                            #{"ee" "ff"}])
+           (anagram/two-words-of-same-size-as "aaaa"
+                                              #{"bbbbb" "ccc" "d" "ee" "ff"})))))
 
 (deftest is-anagram?-test
   (testing "return true when is an anagram"
-    (is (true? (is-anagram? "limbo" "mobil")))
-    (is (true? (is-anagram? "limbo" "bolim")))
-    (is (false? (is-anagram? "limbo" "limbu")))
-    (is (false? (is-anagram? "limbo" "limbou")))))
+    (is (true? (anagram/is-anagram? "limbo" "mobil")))
+    (is (true? (anagram/is-anagram? "limbo" "bolim")))
+    (is (false? (anagram/is-anagram? "limbo" "limbu")))
+    (is (false? (anagram/is-anagram? "limbo" "limbou")))))
 
 (deftest anagram-test
   (testing "inverting anagram"
